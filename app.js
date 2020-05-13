2cc0090a4a54fc9705fe1091e2eb73d0bac61351
ShaneMit <shanemit@cox.net>
2020-05-13 11:56:12 -0700

    Added pseudo code and npms

---
+++ app.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer')
+
+// Prompt user for 'New Game' or 'Leaderboard'
+
+// display Categories when New Game is picked
+
+// Prompt a series of 10 questions 
+
+// If they are correct, tell them they were successful
+
+// If they are incorrect, display a message that says they were incorrect and display the right answer
+
+// Once all questions are done, display their total score
+
+// Prompt user for a username to record their high score
